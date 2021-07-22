69e3e4c478b8d2821d49e77933715609e339b593
Ricardo <ricardo.pizzimenti@hotmail.com>
2021-07-22 10:04:02 -0300

    Multiple players option added
    Now you can play up to 4 players

--- main.py
+++ main.py
@@ -1,59 +1,20 @@
-from random import randint, choice
-from animals import animals_question
-from geography import geography_question
-from movies import movies_question
-from music import music_question
+from game import game
 
-score = 0
-tries = 3
-question_cat_num = '0'
+num_players = int(input('How many player are going to play? '))
 
-while question_cat_num not in '12345':
-    question_cat_num = str(input('What category of question do you want to answer?\n'
-                                 '1 - Geography\n'
-                                 '2 - Animals\n'
-                                 '3 - Music\n'
-                                 '4 - Movies\n'
-                                 '5 - Random\n'))
-    if question_cat_num not in '12345':
-        print('You must choose one of the listed options\n')
+if num_players not in range(1, 4):
+    while num_players not in range(1, 4):
+        print('\nYou can play solo or up to 4 players.')
+        num_players = int(input('How many player are going to play? '))
 
-while tries > 0:
+points = {}
+for i in range(1, num_players + 1):
+    player_name = input(f'\nWhat´s the player {i} name? ')
+    points[player_name] = game(player_name)
 
-    if question_cat_num == '1':
-        question_cat = geography_question
-    elif question_cat_num == '2':
-        question_cat = animals_question
-    elif question_cat_num == '3':
-        question_cat = music_question
-    elif question_cat_num == '4':
-        question_cat = movies_question
-    elif question_cat_num == '5':
-        question_cat = choice([geography_question, animals_question, music_question, movies_question])
+winner = [i for i, e in points.items() if e == max(points.values())]
 
-    rand_questions = list(question_cat)
-    rand_answer = list(question_cat.values())
-    question_number = randint(0, (len(rand_questions) - 1))
-
-    print(rand_questions[question_number])
-    guess = input(str('\nWhat is your guess?\n'))
-
-    while guess not in 'abcd':
-        print('You should guess "a", "b", "c" or "d"')
-        guess = input(str('What is your guess?\n'))
-
-    if guess == rand_answer[question_number]:
-        print('You got it!')
-        score += 1
-        tries -= 1
-    else:
-        print('Oh no! You got it wrong...')
-        tries -= 1
-
-    question_cat.pop(f'{rand_questions[question_number]}')
-
-    if tries == 0:
-        print(f'\nYour final scores is {score}')
-    else:
-        print(f'\nYou have {tries} more questions to answer')
-        print(f'Your current score is {score}\n')
+if len(winner) == 1:
+    print(f'\nThe winner with {max(points.values())} points is {"".join(winner)}!!!')
+else:
+    print(f'\nIt´s a tie! The winners are {", ".join(winner[0:-1])} and {"".join(winner[-1:])}!')
